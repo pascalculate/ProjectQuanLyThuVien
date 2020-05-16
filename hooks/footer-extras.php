5e8f6dc089d91f66a19db5b12db29548f90ba0b1
nguyenkhieu20168756 <nguyen.kv168756@sis.hust.edu.vn>
2020-05-16 22:15:46 +0700

    Them Magazines

---
+++ hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>Bach Khoa Libray Management System &nbsp;<a href="https://api.whatsapp.com/send?phone=+254708344101">Developed By KNDN</a>.</strong>
+  </footer>
